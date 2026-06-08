6918b2425c29e02b742389632fbe2d4b6de55b68
Luca Garofalo <assistenza@thedigitalworld.it>
2026-06-08 17:29:33 +0200

    upd

--- assets/CookiePage-d2FG6H3E.js
+++ assets/CookiePage-d2FG6H3E.js
@@ -1,1 +1,1 @@
-import{u as t,j as a}from"./index-DlhfGgwz.js";import{L as e}from"./LegalLayout-UzqGeK4f.js";function n(){const o=t();return a.jsx(e,{data:o.legalPages.cookie})}export{n as default};
+import{u as t,j as a}from"./index-9244604e.js";import{L as e}from"./LegalLayout-UzqGeK4f.js";function n(){const o=t();return a.jsx(e,{data:o.legalPages.cookie})}export{n as default};
--- assets/PrivacyPage-BcZ3x0D4.js
+++ assets/PrivacyPage-BcZ3x0D4.js
@@ -1,1 +1,1 @@
-import{u as t,j as e}from"./index-DlhfGgwz.js";import{L as o}from"./LegalLayout-UzqGeK4f.js";function n(){const a=t();return e.jsx(o,{data:a.legalPages.privacy})}export{n as default};
+import{u as t,j as e}from"./index-9244604e.js";import{L as o}from"./LegalLayout-UzqGeK4f.js";function n(){const a=t();return e.jsx(o,{data:a.legalPages.privacy})}export{n as default};
