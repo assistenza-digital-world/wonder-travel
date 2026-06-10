7675fcee30064067fa579b999c5f7bfefdb84e51
Luca Garofalo <assistenza@thedigitalworld.it>
2026-06-10 14:45:33 +0200

    upd

---
+++ assets/CookiePage-d8a3825a.js
@@ -0,0 +1,1 @@
+import{u as t,j as a}from"./index-66529911.js";import{L as e}from"./LegalLayout-f8cce51a.js";function n(){const o=t();return a.jsx(e,{data:o.legalPages.cookie})}export{n as default};
---
+++ assets/PrivacyPage-9b6da6e0.js
@@ -0,0 +1,1 @@
+import{u as t,j as e}from"./index-66529911.js";import{L as o}from"./LegalLayout-f8cce51a.js";function n(){const a=t();return e.jsx(o,{data:a.legalPages.privacy})}export{n as default};
